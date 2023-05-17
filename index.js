e4a021c2d9c92d67a198a09c6ac44fe7eb274b0e
Merch04 <kirill004mel@mail.ru>
2023-05-17 21:43:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(errorHandler)
 const start = async () => {
   try {
     await sequelize.authenticate()
-    await sequelize.sync({ force: true })
+    await sequelize.sync()
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`)
     })
